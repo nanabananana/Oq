cb268ef7d7acd3c35f54eaa4faad7a2c7f6e8ed5
day <rakhmatullinadayana@gmail.com>
2023-11-20 00:24:43 +0600

    design

---
+++ main.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Retrieve user information from local storage
+    const userData = JSON.parse(localStorage.getItem('loggedInUser'));
+
+    // Display user information on the main page
+    if (userData) {
+        const accountPhoto = document.getElementById('account-photo');
+        const accountName = document.getElementById('account-name');
+
+        // Update the main page elements with user information
+        accountName.textContent = userData.email; // You can use any user-related data here
+        // For simplicity, use a placeholder image
+        accountPhoto.src = "placeholder.jpg";
+        accountPhoto.alt = "Account Photo";
+    }
+});
